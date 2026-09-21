bb416de3658a193ca4fa8f430a998591ed17904a
Mohammed wasi <codeflow1702@gmail.com>
2026-09-21 23:36:58 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,20 @@
+// Import Firebase SDKs
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, onAuthStateChanged, signOut, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore, collection, getDocs, doc, getDoc, addDoc, updateDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+// Your Config
+const firebaseConfig = {
+    apiKey: "AIzaSyDLCNAA-bbUo2EQfp1EDfGE76b8iSQmCTI",
+    authDomain: "bharat-c734b.firebaseapp.com",
+    projectId: "bharat-c734b",
+    appId: "1:273763763790:web:b2499eff81a4b5b5c3651f"
+};
+
+// Initialize
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+// Export for use in other files
+export { app, auth, db, onAuthStateChanged, signOut, signInWithEmailAndPassword, createUserWithEmailAndPassword, collection, getDocs, doc, getDoc, addDoc, updateDoc, deleteDoc };
